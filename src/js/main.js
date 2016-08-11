18bc007aee77923fada74f7803b06e9de7c7c1b6
Elif Koc <elifkoc@nyu.edu>
2016-08-10 19:09:06 -0700

    Styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,78 +1,93 @@
-// require("./lib/social");
+require("./lib/social");
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
 var app = require("./application");
 
-app.controller("prediction", prediction);
                
-function prediction($scope) {
-  $scope.user = {
-    dolphins0911: {
-      won: false,
-      spread: 0
-    },
-    fortyniners0925: {
-      won: false,
-      spread: 0
-    },
-    jets1002: {
-      won: false,
-      spread: 0
-    },
-    falcons1016: {
-      won: false,
-      spread: 0
-    },
-    cardinals1023: {
-      won: false,
-      spread: 0
-    },
-    saints1030: {
-      won: false,
-      spread: 0
-    },
-    bills1107: {
-      won: false,
-      spread: 0
-    },
-    patriots1113: {
-      won: false,
-      spread: 0
-    },
-    eagles1120: {
-      won: false,
-      spread: 0
-    },
-    buccaneers1127: {
-      won: false,
-      spread: 0
-    },
-    panthers1204: {
-      won: false,
-      spread: 0
-    },
-    packers1211: {
-      won: false,
-      spread: 0
-    },
-    rams1215: {
-      won: false,
-      spread: 0
-    },
-    cardinals1224: {
-      won: false,
-      spread: 0
-    },
-    fortyniners0101: {
-      won: false,
-      spread: 0
-    }
-    
-  }
-  $scope.score1 = 1;
-  $scope.score2 = 2;
-}
+var prediction = function($scope) {
+  
+  $scope.games = window.games;
+  var person = "user";
 
+  var restore = function(source) {
+    source.games.forEach(function(f) {
+      $scope.games.forEach(function(g) {
+        if (f.id == g.id) {
+          console.log("yes");
+          g.win = f.win;
+          g.spread = f.spread;
+        }
+      });
+    });
+  }
+  
+  var saved = null;
+  
+  var save = function() {
+    saved = $scope.games.map(function(g) {
+      return {
+        win: g.win,
+        spread: g.spread,
+        id: g.id
+      }
+    })
+  };
+  
+  $scope.switchView = function(name) {
+  
+        $scope.msg = 'clicked';
+
+    if (person == "user") {
+      save();
+      console.log('we are on user');
+    }
+    if (name == "user") {
+      restore(saved);
+            console.log('we want to restore user');
+
+    } else {
+      //load preset
+            console.log('bob preset');
+
+    }
+  }
+  
+  var hash = window.location.hash.replace(/^#/, "");
+  if (hash) {
+        console.log("yes hash");
+
+    var fromURL = JSON.parse(decodeURIComponent(hash));
+    if (fromURL.games) restore(fromURL);
+  } else {
+    console.log("no hash");
+    var fromLocal = localStorage.getItem("hawks-prediction");
+    if (fromLocal) {
+      fromLocal = fromLocal.replace(/^#/, "");
+      fromLocal = JSON.parse(decodeURIComponent(fromLocal));
+      restore(fromLocal);
+    }
+  }
+  
+  $scope.$watch(function() {
+    //if (person != "user") return;
+    var filtered = $scope.games.map(function(entry) {
+      return {
+        id: entry.id,
+        win: entry.win,
+        spread: entry.spread * 1
+      }
+    });
+    
+    var json = JSON.stringify({ games: filtered });
+    var encoded = "#" + encodeURIComponent(json);
+    history.replaceState(encoded, encoded, encoded);
+    //I think the problem is here at local storage. It thinks 0 is valid. Would a submit button solve this?
+    localStorage.setItem("hawks-prediction", encoded);
+  });
+
+}
 prediction.$inject = ["$scope"];
+
+app.controller("prediction", prediction);
 
