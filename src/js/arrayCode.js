874b6e93f41f010cd584dee0b5886a61ecf3eafe
Thomas Wilburn <news@thomaswilburn.net>
2016-08-11 10:58:39 -0700

    Added docs, converted to an actual base-N system

--- src/js/arrayCode.js
+++ src/js/arrayCode.js
@@ -2,14 +2,15 @@
 
 This module packs an array of objects into a string of text. In order to
 achieve very small packed data sizes, all the objects must only contain
-numeric values, and the keys must be provided during both packing and
+integer values from 0 to n^2, where n is the size of the cipher character
+space (currently 65), and the keys must be provided during both packing and
 unpacking. Packed strings are forward compatible, as long as you only add keys
 onto the end of the list when packing/unpacking.
 
-A packed string always starts with two numbers. The first number tells the
-decoder how many keys are encoded per object (the stride). The second number
-indicates the width of each value. The remaining text is the actual data from
-the array items.
+A packed string always starts with two numbers. The first is a flag value that
+tells the decoder how many keys are encoded per object (the stride). The
+second tells the decoder how many digits each value should contain. In
+practice, this is almost always 1 or 2.
 
 Because this coding can only encode arrays containing objects with numerical
 values, it can't be used to save metadata, such as the user's name or the
@@ -25,34 +26,54 @@ var cipher = "-_.~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 
 var pack = function(keys, data) {
   var buffer = [];
+  //store all data in a flat array as an integer
   data.forEach(function(d) {
-    keys.forEach(k => buffer.push((d[k] || 0) * 1));
+    keys.forEach(k => buffer.push(d[k] | 0));
   });
+  // find the highest value in the buffer
   var high = Math.max.apply(null, buffer);
+  // find the highest possible single-character value (65)
   var max = cipher.length - 1;
-  var size = Math.floor(high / max) + 1;
+  // compute how many "digits" we need to express the highest value
+  var size = 1;
+  while (Math.pow(65, size) < high) size++;
+  // convert the buffer into strings of digits
   var out = buffer.map(function(v) {
-    var overflow = Math.floor(v / max);
-    var last = v % max;
-    var repeated = new Array(overflow + 1).join(cipher[max]);
-    var padding = new Array(size - overflow).join(cipher[0]);
-    var packed = padding + cipher[last] + repeated;
+    var packed = "";
+    //step down through the digits and create the converted base-N number
+    for (var i = size - 1; i > 0; i--) {
+      var digit = Math.floor(v / Math.pow(max, i));
+      packed += cipher[digit];
+      v -= digit * Math.pow(max, i);
+    }
+    packed += cipher[v];
     return packed;
   });
+  // add the packing metadata onto the front of the array
   return keys.length.toString() + size + out.join("");
 };
 
 var unpack = function(keys, packed) {
   var output = [];
+  //extract the metadata and packed array
   var stride = packed[0];
   var width = packed[1];
+  var max = cipher.length - 1;
   var data = packed.slice(2);
-  for (var i = 0; i < data.length; i += stride * width) {
+  //walk through the values
+  for (var cell = 0; cell < data.length; cell += stride * width) {
     var item = {};
+    // extract the values for each key
     for (var j = 0; j < stride; j++) {
       var key = keys[j];
-      var value = data.substr(i + j * width, width).split("").map(c => cipher.indexOf(c)).reduce((p, n) => n + p, 0);
-      item[key] = value;
+      //get the numerical values for the individual digits in each value
+      var digits = data.substr(cell + j * width, width).split("").map(n => cipher.indexOf(n));
+      //turn those digits into a summed number
+      //i.e., [2, 3] in base-65 is 2 * 65 + 3
+      var values = digits.map((n, i) => n * Math.pow(max, width - i - 1));
+      var sum = values.reduce((a, b) => a + b, 0);
+      //set the key/value on the object
+      item[key] = sum;
     }
     output.push(item);
   }
--- src/js/main.js
+++ src/js/main.js
@@ -2,28 +2,10 @@ require("./lib/social");
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
+var qs = require("querystring");
 var app = require("./application");
 var ascii = require("./arrayCode");
 var aKeys = "win h a".split(" ");
-
-var parseQuery = function(qs) {
-  var out = {};
-  var parts = qs.split("&");
-  parts.forEach(function(p) {
-    var [key, value] = p.split("=");
-    out[key] = value;
-  });
-  return out;
-}
-
-var encodeQuery = function(data) {
-  var qs = [];
-  for (var k in data) {
-    qs.push(k + "=" + data[k]);
-  }
-  return qs.join("&");
-}
-
                
 var prediction = function($scope) {
   
@@ -33,33 +15,19 @@ var prediction = function($scope) {
   var restore = function(source) {
     source.forEach(function(g, i) {
       var game = $scope.games[i];
-      var winner = null;
       if (g.win > 0) {
-        if (g.win == 1) {
-          winner = "seahawks";
-        } else {
-          winner = game.away == "seahawks" ? game.home : game.away;
-        }
+        game.winner = g.win == 1 ? game.home : game.away;
       }
       
       game.a = g.a;
       game.h = g.h;
-      game.winner = winner;
     });
   }
   
   var saved = null;
   
   var save = function() {
-    saved = $scope.games.map(function(g) {
-      return {
-        id: g.id,
-        win: g.win,
-        //home score and away score
-        h: g.h,
-        a: g.a
-      }
-    })
+    saved = $scope.games.slice();
   };
   
   $scope.switchView = function(name) {
@@ -72,17 +40,16 @@ var prediction = function($scope) {
     }
     if (name == "user") {
       restore(saved);
-            console.log('we want to restore user');
-
+      console.log('we want to restore user');
     } else {
       //load preset
-            console.log('bob preset');
-
+      console.log('bob preset');
     }
+    person = name;
   }
   
   var hash = window.location.hash.replace(/^#/, "");
-  var query = parseQuery(hash);
+  var query = qs.decode(hash);
   if (query.games) {
     var fromURL = ascii.unpack(aKeys, query.games);
     if (fromURL) restore(fromURL);
@@ -96,20 +63,25 @@ var prediction = function($scope) {
   
   $scope.$watch(function() {
     var filtered = $scope.games.map(function(entry) {
-      //limit scores to 99 at highest
-      if (entry.a > 99) entry.a = 99;
-      if (entry.h > 99) entry.h = 99;
       return {
         id: entry.id,
-        // SEA is 1, opponent is 2, tie is 0
-        win: entry.winner ? entry.winner == "seahawks" ? 1 : 2 : 0,
+        // home is 1, away is 2, tie is 0
+        win: entry.winner ? entry.winner == entry.home ? 1 : 2 : 0,
         //home score and away score
         h: entry.h,
         a: entry.a
       }
     });
 
-    var encoded = "#" + encodeQuery({
+    // remove trailing items with no data
+    for (var i = filtered.length - 1; i >= 0; i--) {
+      var item = filtered[i];
+      if (!item.win && !item.h && !item.a) {
+        filtered.pop();
+      }
+    }
+
+    var encoded = "#" + qs.encode({
       games: ascii.pack(aKeys, filtered),
       timestamp: Date.now()
     });
